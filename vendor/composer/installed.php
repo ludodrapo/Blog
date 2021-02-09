4d2856af32919aae203bb26b89f2b33825014557
ludodrapo <72028967+ludodrapo@users.noreply.github.com>
2021-02-09 10:39:52 +0100

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'tinymce/tinymce' => 
+    array (
+      'pretty_version' => '5.6.2',
+      'version' => '5.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '310051defb85fec8581ace8b739ab7b18023db8a',
+    ),
+  ),
+);
