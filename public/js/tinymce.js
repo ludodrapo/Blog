ec3ae8938918702d37d03f0e6c7620a8e770a470
ludodrapo <72028967+ludodrapo@users.noreply.github.com>
2021-02-09 10:28:17 +0100

    Assets

---
+++ public/js/tinymce.js
@@ -0,0 +1,9 @@
+//Script a bit light only for posts content edition
+
+tinymce.init({
+    selector: '#editor',
+    plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    toolbar_mode: 'floating',
+    min_height: 400,           
+    autoresize_bottom_margin: 16
+});
