c0dc54ec8ff7850b8a543e2ea5d74dad50ffe449
ludodrapo <72028967+ludodrapo@users.noreply.github.com>
2021-02-09 09:55:13 +0100

    Delete backendHomepage.php

--- Blog copie au 03 02 2021/views/backend/backendHomepage.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); ?>
-
-<?php ob_start(); ?>
-
-<h2 class="center">Bienvenue sur la page d'administration du blog</h2>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require('backendTemplate.php'); ?>
