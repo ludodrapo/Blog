6518e94ea9d05c45451e50ddec3b071b55360081
ludodrapo <72028967+ludodrapo@users.noreply.github.com>
2021-02-17 12:50:05 +0100

    Another try to fix the backend index

--- backendIndex.php
+++ backendIndex.php
@@ -2,29 +2,29 @@
 
 session_start();
 
-require('controller/backend/backendController.php');
+require 'controller/backend/backendController.php';
 
 try
 {
-	if (isset($_GET['action']))
+	if (isset($_GET['action']) && !empty($_GET['action']))
 	{
-		if ($_GET['action'] == 'displayAllPosts')
+		if ($_GET['action'] === 'displayAllPosts')
 		{
 			displayAllPosts();
 		}
-		elseif ($_GET['action'] ==  'displayAwaitingComments')
+		elseif ($_GET['action'] ===  'displayAwaitingComments')
 		{
 			displayAwaitingComments();
 		}
-		elseif ($_GET['action'] == 'newPost')
+		elseif ($_GET['action'] === 'newPost')
 		{
-			require('views/backend/backendNewPost.php');
+			require 'views/backend/backendNewPost.php';
 		}
-		elseif ($_GET['action'] == 'displayAllSubscribers')
+		elseif ($_GET['action'] === 'displayAllSubscribers')
 		{
 			displayAllSubscribers();
 		}
-		elseif ($_GET['action'] == 'addPost')
+		elseif ($_GET['action'] === 'addPost')
 		{
 			if (
 				isset($_POST['title']) && !empty($_POST['title']) &&
@@ -36,27 +36,21 @@ try
 				addNewPost(
 					strip_tags($_POST['title']),
 					strip_tags($_POST['lead']),
-					mysqli_real_escape_string($_POST['content']),
+					htmlspecialchars($_POST['content']),
 					strip_tags($_POST['category'])
 				);
 			}
-			else
-			{
-				throw new Exception("l manque des éléments nécessaires à l'enregistrement de l'article.", 1);
-			}
+			throw new Exception("l manque des éléments nécessaires à l'enregistrement de l'article.", 1);
 		}
-		elseif ($_GET['action'] == 'fillUpdatePostPage')
+		elseif ($_GET['action'] === 'fillUpdatePostPage')
 		{
-			if (!isset($_GET['post_id']))
+			if (!isset($_GET['post_id']) || empty($_GET['post_id']))
 			{
 				throw new Exception("Pas de numéro de post identifié, désolé.", 1);
 			}
-			else
-			{
-				fillUpdatePostPage(strip_tags($_GET['post_id']));				
-			}
+			fillUpdatePostPage(strip_tags($_GET['post_id']));			
 		}
-		elseif ($_GET['action'] == 'updatePost')
+		elseif ($_GET['action'] === 'updatePost')
 		{
 			if (
 				isset($_POST['title']) && !empty($_POST['title']) &&
@@ -74,95 +68,66 @@ try
 					strip_tags($_POST['post_id'])
 				);
 			}
-			else
-			{
-				throw new Exception("Il manque un ou plusieurs éléments nécessaires à la modification de l'article, désolé.", 1);
-			}
+			throw new Exception("Il manque un ou plusieurs éléments nécessaires à la modification de l'article, désolé.", 1);
 		}
-		elseif ($_GET['action'] == 'validateComment')
+		elseif ($_GET['action'] === 'validateComment')
 		{
-			if (!isset($_GET['comment_id']))
+			if (!isset($_GET['comment_id']) || empty($_GET['comment_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu le commentaire à valider, désolé.", 1);
 			}
-			else
-			{
-				validateComment(strip_tags($_GET['comment_id']));
-			}
+			validateComment(strip_tags($_GET['comment_id']));
 		}
-		elseif ($_GET['action'] == 'deleteComment')
+		elseif ($_GET['action'] === 'deleteComment')
 		{
 			if (!isset($_GET['comment_id']) || empty($_GET['comment_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu le commentaire à supprimer.", 1);
 			}
-			else
-			{
-				deleteComment(strip_tags($_GET['comment_id']));
-			}
+			deleteComment(strip_tags($_GET['comment_id']));
 		}
-		elseif ($_GET['action'] == 'activatePost')
+		elseif ($_GET['action'] === 'activatePost')
 		{
-			if (isset($_GET['post_id']))
-			{
-				activatePost(strip_tags($_GET['post_id']));
-			}
-			else
+			if (!isset($_GET['post_id']) || empty($_GET['post_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu l'article à activer", 1);
 			}
+			activatePost(strip_tags($_GET['post_id']));
 		}
-		elseif ($_GET['action'] == 'deactivatePost')
+		elseif ($_GET['action'] === 'deactivatePost')
 		{
-			if (isset($_GET['post_id']))
-			{
-				deactivatePost(strip_tags($_GET['post_id']));
-			}
-			else
+			if (!isset($_GET['post_id']) || empty($_GET['post_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu l'article à désactiver", 1);
 			}
+			deactivatePost(strip_tags($_GET['post_id']));	
 		}
-		elseif ($_GET['action'] == 'blockUser')
+		elseif ($_GET['action'] === 'blockUser')
 		{
-			if (isset($_GET['user_id']))
-			{
-				blockUser(strip_tags($_GET['user_id']));
-			}
-			else
+			if (!isset($_GET['user_id']) || empty($_GET['user_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu l'identifiant de l'utilisateur à bloquer.", 1);
 			}
+			blockUser(strip_tags($_GET['user_id']));
 		}
-		elseif ($_GET['action'] == 'authorizeUser')
+		elseif ($_GET['action'] === 'authorizeUser')
 		{
-			if (isset($_GET['user_id']))
-			{
-				authorizeUser(strip_tags($_GET['user_id']));
-			}
-			else
+			if (!isset($_GET['user_id']) || empty($_GET['user_id']))
 			{
 				throw new Exception("Nous n'avons pas reconnu l'idendtifiant de l'utilisateur à réactiver.", 1);
 			}
+			authorizeUser(strip_tags($_GET['user_id']));
 		}
-		elseif ($_GET['action'] == 'uploadImage')
+		elseif ($_GET['action'] === 'uploadImage')
 		{
 			if (!isset($_FILES['image_to_upload']) || empty($_FILES['image_to_upload']))
 			{
 				throw new Exception("Aucune image n'a été sélectionnée", 1);
 			}
-			else
-			{
-				uploadImage($_FILES['image_to_upload']);
-			}
+			uploadImage($_FILES['image_to_upload']);
 		}
 	}
-
-	else
-	{		
-		fillBackendHomepage();
-	}
-
+	fillBackendHomepage();
 }
 catch(Exception $e)
 {
