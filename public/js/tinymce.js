711181a5bb1a0675fde0fdb45fa28690c2a0d2d5
ludodrapo <72028967+ludodrapo@users.noreply.github.com>
2021-02-17 11:04:04 +0100

    Fixed js codacy issues

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -1,10 +1,10 @@
 //Script a bit light only for posts content edition
 
 tinymce.init({
-    selector: '#editor',
+    selector: "#editor",
     branding: false,
-    plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    toolbar_mode: 'floating',
+    plugins: "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+    toolbar_mode: "floating",
     min_height: 400,           
     autoresize_bottom_margin: 16
 });
